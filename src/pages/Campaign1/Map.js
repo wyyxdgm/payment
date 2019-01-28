b16b9645edf469da9d1f974d0cca43d5cb6d762c
李尧 <regreceive@163.com>
2019-01-28 18:22:14 +0800

    地图信息覆盖物

--- src/pages/Campaign1/Map.js
+++ src/pages/Campaign1/Map.js
@@ -5,6 +5,44 @@ import isEqual from 'lodash/isEqual';
 import styles from './style.less';
 
 const { qq } = window;
+const persons = [
+  { lastName: '张', tel: '137****5236' },
+  { lastName: '王', tel: '138****0918' },
+  { lastName: '李', tel: '159****0021' },
+  { lastName: '赵', tel: '185****2907' },
+  { lastName: '陈', tel: '177****6652' },
+];
+
+function Info(opts) {
+  qq.maps.Overlay.call(this, opts);
+}
+Info.prototype = new qq.maps.Overlay();
+
+Info.prototype.construct = function() {
+  this.dom = document.createElement('div');
+  this.dom.className = 'mapInfo';
+  this.dom.innerHTML = this.get('inithtml');
+  this.getPanes().floatPane.appendChild(this.dom);
+};
+
+Info.prototype.html = function(html) {
+  this.dom.innerHTML = html;
+};
+
+Info.prototype.draw = function() {
+  const position = this.get('position');
+  if (position) {
+    const pixel = this.getProjection().fromLatLngToDivPixel(position);
+    console.log(this.dom);
+    this.dom.style.left = `${pixel.getX() - 145 / 2}px`;
+    this.dom.style.top = `${pixel.getY() - 80}px`;
+  }
+};
+
+Info.prototype.destroy = function() {
+  this.dom.parentNode.removeChild(this.dom);
+};
+
 export default class Map extends PureComponent {
   static propTypes = {
     onChange: PropTypes.func,
@@ -54,7 +92,7 @@ export default class Map extends PureComponent {
       const { latitude, longitude } = nextProps.value;
       const center = new qq.maps.LatLng(latitude, longitude);
       this.map.panTo(center);
-      this.map.zoomTo(13);
+      this.map.zoomTo(12);
 
       this.geocoder.getAddress(center);
 
@@ -63,7 +101,7 @@ export default class Map extends PureComponent {
         center,
         fillColor: new qq.maps.Color(51, 102, 204, 0.2),
         strokeWeight: 0,
-        radius: 1000,
+        radius: 1500,
       });
       setTimeout(() => {
         const marker = new qq.maps.Marker({
@@ -72,6 +110,14 @@ export default class Map extends PureComponent {
           map: this.map,
         });
       }, 1000);
+
+      const person = persons[Math.floor(Math.random() * 5)];
+
+      this.infoOverlay = new Info({
+        map: this.map,
+        position: center,
+        inithtml: `<p>${person.lastName}** 刚刚浏览了您的园所</p><p>${person.tel}</p>`,
+      });
     }
   }
 
